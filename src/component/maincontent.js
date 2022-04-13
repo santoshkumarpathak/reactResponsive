220df7a664de0e488b9bcbf4dabe990014a7a212
santohsTECTUM <santosh.tectum@gmail.com>
2022-04-13 10:53:59 +0530

    merge it

--- src/component/maincontent.js
+++ src/component/maincontent.js
@@ -7,13 +7,17 @@ import {
 } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core"
 import image from '../static/img.jpg';
-const style = makeStyles((theme) => ({
-import { makeStyles } from "@material-ui/core"
+
+
+
+
 const style = makeStyles((theme) =>({
    container: {
         padding: theme.spacing(10)
-    }
-}))
+    },
+}));
+
+
 const Maincontent = () => {
     const classes = style()
     return (
